024d5231625b8ed1a60354800e12e1781312c407
SangHun <shzero211@naver.com>
2022-02-06 23:57:28 +0900

    2022/02/06

---
+++ coding-Test/src/Level2/모음사전.java
@@ -0,0 +1,14 @@
+package Level2;
+
+public class 모음사전 {
+	public int solution(String word) {
+		String str = "AEIOU";
+		int[] x = {781,156,31,6,1};
+		int index,result=word.length();
+		for(int i=0;i<word.length();i++){
+			index = str.indexOf(word.charAt(i));
+			result+=x[i]*index;
+		}
+		return result;
+	}
+}
